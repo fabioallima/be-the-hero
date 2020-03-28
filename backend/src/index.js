0e3373313278b25da57cab8eead4b270ad1f9d70
Lima, Fabio Almeida <fabio.almeida.lima@accenture.com>
2020-03-28 14:53:39 -0300

    Backend da aplicação implementado

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+//01:21:00 min
+
+
+
+app.listen(3333);
